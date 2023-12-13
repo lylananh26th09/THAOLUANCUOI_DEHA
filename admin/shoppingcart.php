36e491fabe717fb481a7eb9608b90349aa353f5e
lylananh26th09 <97857690+lylananh26th09@users.noreply.github.com>
2023-12-13 22:08:40 +0700

    Add files via upload

---
+++ admin/shoppingcart.php
@@ -0,0 +1,11 @@
+<?php
+class CartItem {
+    public $ten_sp;
+    public $gia_sp;
+
+    public function __construct($ten_sp, $gia_sp) {
+        $this->ten_sp = $ten_sp;
+        $this->gia_sp = $gia_sp;
+    }
+}
+?>
